2bd7ea272e14c043b3df9f3ac43fb02bec10a74e
kazemlraq-glitch <kazemlraq@gmail.com>
2026-02-06 23:30:46 +0300

    Update supabase_config.js

--- supabase_config.js
+++ supabase_config.js
@@ -1,7 +1,7 @@
 // supabase_config.js
 // استبدل الروابط أدناه بروابط مشروعك الخاص لكل عميل
 const SUPABASE_URL = "https://qvfvdystksqzmzugddvj.supabase.co";
-const SUPABASE_KEY = "sb_publishable_C8M1nnjNS5T6cp9ImtoE1A_HQPGCw2i";
+const SUPABASE_KEY = "qvfvdystksqzmzugddvj";
 
 // تهيئة العميل (يجب تضمين مكتبة supabase في الـ index.html)
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
