cff7a8190f718db93ffceb979db61c1e458a332d
kazemlraq-glitch <kazemlraq@gmail.com>
2026-02-07 00:47:41 +0300

    Update supabase_config.js

--- supabase_config.js
+++ supabase_config.js
@@ -1,11 +1,10 @@
 // supabase_config.js
-// 1. رابط المشروع الخاص بك (من الصورة الثالثة)
+
 const SUPABASE_URL = "https://qvfvdystksqzmzugddvj.supabase.co";
 
-// 2. المفتاح العام الخاص بك (من الصورة الثالثة)
+// المفتاح الجديد الخاص بك (صحيح 100%)
 const SUPABASE_KEY = "sb_publishable_C8M1nnjNS5T6cp9ImtoE1A_HQPGCw2i";
 
-// تهيئة الاتصال بقاعدة البيانات
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-console.log("تم تحميل إعدادات Supabase بنجاح - نسخة كاظم البهادلي");
+console.log("تم تحميل الإعدادات - مفتاح Publishable الجديد");
